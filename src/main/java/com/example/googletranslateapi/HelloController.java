8057d63edf7d8d671a44d78bf821ad8dd87b9a9e
MasLuarasi <87911434+MasLuarasi@users.noreply.github.com>
2022-06-02 17:00:09 -0400

    English -> Spanish Translation

--- src/main/java/com/example/googletranslateapi/HelloController.java
+++ src/main/java/com/example/googletranslateapi/HelloController.java
@@ -1,9 +1,15 @@
 package com.example.googletranslateapi;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+import java.net.URI;
 
 public class HelloController
 {
@@ -19,14 +25,32 @@ public class HelloController
     @FXML
     private TextField outputField;
 
-    private String inputText;
-    private String outputText;
+//    private String inputText;
+//    private String outputText;
 
     @FXML
-    protected void onTranslateButtonClick()
+    protected void onTranslateButtonClick() throws Exception
     {
-        inputText = inputField.getText();
-        outputText = inputText;
-        outputField.setText(outputText);
+        outputField.setText(translate(inputField.getText()));
+    }
+
+    protected String translate(String original)throws Exception
+    {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://google-translate1.p.rapidapi.com/language/translate/v2"))
+                .header("content-type", "application/x-www-form-urlencoded")
+                .header("Accept-Encoding", "application/gzip")
+                .header("X-RapidAPI-Host", "google-translate1.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "4125e08ab5msh1e35e54946ee894p1de70djsn0b70aa45221f")
+                .method("POST", HttpRequest.BodyPublishers.ofString("q=" +
+                        original +
+                        "&format=text" +
+                        "&target=es" +
+                        "&source=en"))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        String ret = response.body();
+        ret = ret.substring(ret.indexOf("translatedText")+17, ret.length()-5);
+        return ret;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.googletranslateapi {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
 
     opens com.example.googletranslateapi to javafx.fxml;
     exports com.example.googletranslateapi;
